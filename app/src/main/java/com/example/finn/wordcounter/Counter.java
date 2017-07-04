7845349edc80cd00ff7f4091913ea7fab21a64cc
finnporter <iamfinnporter@gmail.com>
2017-07-04 18:20:20 +0100

    initial commit

---
+++ app/src/main/java/com/example/finn/wordcounter/Counter.java
@@ -0,0 +1,8 @@
+package com.example.finn.wordcounter;
+
+/**
+ * Created by Finn on 04/07/2017.
+ */
+
+public class Counter {
+}
---
+++ app/src/test/java/com/example/finn/wordcounter/CounterTest.java
@@ -0,0 +1,9 @@
+package com.example.finn.wordcounter;
+
+/**
+ * Created by Finn on 04/07/2017.
+ */
+
+public class CounterTest {
+    private
+}
